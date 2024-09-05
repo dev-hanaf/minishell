d1da0330dbeea0db9c5ef5003072bedd3411e7d1
zakaria <zakariamourid10@gmail.com>
2024-09-05 14:53:17 +0100

    fixed signals

--- src/signals/signals.c
+++ src/signals/signals.c
@@ -8,12 +8,6 @@ void sigHandler(int sig)
 	get_ms()->status = sig + 128;
 	get_env_ld(get_ms()->env_ld,"?")->value = ft_itoa(get_ms()->status);
 }
-void sigHandler_child(int sig)
-{
-	ft_putstr_fd("alooohaaaaaaaaaa",2);
-	dprintf(2,"^\\Quit (core dumped)\n");
-	exit(sig + 1000);
-}
 void handle_signals(void)
 {
 	signal(SIGINT,sigHandler);
@@ -23,9 +17,6 @@ void handle_child_signals(void)
 {
 	signal(SIGINT,SIG_DFL);
 	signal(SIGQUIT,SIG_DFL);
-	// ft_putstr_fd("alooohaaaaaaaaaa22",2);
-	// signal(SIGINT,sigHandler_child);
-	// signal(SIGQUIT,sigHandler_child); 
 }
 
 void sigHandler_parent(int sig)
